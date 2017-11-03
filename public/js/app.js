5c3fa99e13400349c9f4f9fe16346804410d10ac
Ben Lacy <ben.lacy@outlook.com>
2017-11-02 20:40:07 -0500

    sidenav set up

--- public/js/app.js
+++ public/js/app.js
@@ -19,3 +19,13 @@ function displayLowInventory() {
     }
   });
 }
+
+// Set the width of the side navigation to 250px
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Set the width of the side navigation to 0
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
