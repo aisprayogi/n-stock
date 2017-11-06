fcc32a018ca7d05a237bc07706ac1699f634f6ea
Ben Lacy <ben.lacy@outlook.com>
2017-11-06 10:27:58 -0600

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -19,13 +19,3 @@ function displayLowInventory() {
     }
   });
 }
-
-// Set the width of the side navigation to 250px
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-// Set the width of the side navigation to 0
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
